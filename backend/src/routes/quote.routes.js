c96bec152c0d1e90c54333bda151c9a7d462b4f6
Sebastian Mella <seba.mella.isla@gmail.com>
2025-09-21 15:29:05 -0300

    Delete backend directory

--- backend/src/routes/quote.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createQuote } from "../controllers/quote.controller.js";
-
-const router = Router();
-
-// POST /api/quotes → crear nueva cotización
-router.post("/", createQuote);
-
-export default router;
